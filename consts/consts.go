406ab50e59b9445beed0b4dae15582c434e51361
Kundan <131183962+CodeBlocker52@users.noreply.github.com>
2023-09-06 17:26:29 +0000

    Pending changes exported from your codespace

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "token"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "myCustomChain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MCC"
 )
 
 var ID ids.ID
